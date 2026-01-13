8c7a420ed485fbff60f3030ed8acb3a727d66f86
MarioIliescu <355142@via.dk>
2026-01-13 09:07:03 +0100

    removed unused logger

--- src/main/java/via/pro3/slaughterhouse/Start.java
+++ src/main/java/via/pro3/slaughterhouse/Start.java
@@ -1,15 +1,10 @@
 package via.pro3.slaughterhouse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Start {
-
-  private static final Logger log = LoggerFactory.getLogger(Start.class);
-
   public static void main(String[] args) {
     SpringApplication.run(Start.class, args);
     }
